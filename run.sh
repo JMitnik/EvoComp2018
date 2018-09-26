f6d705a5de0dd06d71114ae97d1ab805a115654a
Weitao Luo <luobutang@gmail.com>
2018-09-26 17:48:04 +0200

    Add some new methods for crossover and mutation

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ javac -cp . com/Population.java
 javac -cp . com/EvoAlgorithm.java
 javac -cp contest.jar:. player14.java
 jar cmf MainClass.txt submission.jar player14.class com/Individual.class com/Population.class com/EvoAlgorithm.class
+echo "evaluation=SphereEvaluation:"
 java -jar testrun.jar -submission=player14 -evaluation=SphereEvaluation -seed=1
-# java -jar testrun.jar -submission=player14 -evaluation=BentCigarFunction -seed=1
+echo "evaluation=BentCigarFunction:"
+java -jar testrun.jar -submission=player14 -evaluation=BentCigarFunction -seed=1
 rm player14.class
