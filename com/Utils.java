f2f3d558f5e9eff98d78c26d05648d4d437beb3b
Zardinality <asxaqz2@gmail.com>
2018-09-27 06:23:32 +0800

    extract some shortcut methods, add exp_decay, need tuning

---
+++ com/Utils.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.lang.Math;
+
+class Utils{
+    public static double clamp(double x, double upperBound, double lowerBound) {
+        return Math.min(Math.max(x, lowerBound), upperBound);        
+    }
+    public static double exponentialDecay(double originValue, int timestep, int finalStep) {
+        return (Math.pow(2.71, -timestep/finalStep)-1/2.71)/(1-1/2.71)*originValue;
+    }
+}
--- player14.java
+++ player14.java
@@ -5,92 +5,70 @@ import java.util.Properties;
 import com.Individual;
 import com.Population;
 import com.EvoAlgorithm;
+
 public class player14 implements ContestSubmission {
-Random rnd_;
-ContestEvaluation evaluation_;
-private int evaluations_limit_;
-private Population pop;
-private int popsize;
-public player14() {
-        rnd_ = new Random();
-}
+        Random rnd_;
+        ContestEvaluation evaluation_;
+        private int evaluations_limit_;
+        private Population pop;
+        private int popsize;
 
-public void setSeed(long seed) {
-        // Set seed of algortihms random process
-        rnd_.setSeed(seed);
-}
+        public player14() {
+                rnd_ = new Random();
+        }
 
-public void setEvaluation(ContestEvaluation evaluation) {
-        // Set evaluation problem used in the run
-        evaluation_ = evaluation;
-        // Get evaluation properties
-        Properties props = evaluation.getProperties();
-        // Get evaluation limit
-        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
-        // Property keys depend on specific evaluation
-        // E.g. double param = Double.parseDouble(props.getProperty("property_name"));
-        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
-        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
-        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
-        // Do sth with property values, e.g. specify relevant settings of your algorithm
-        if (isMultimodal) {
-                // Do sth
-        } else {
-                // Do sth else
+        public void setSeed(long seed) {
+                // Set seed of algortihms random process
+                rnd_.setSeed(seed);
         }
-}
-public void InitPopulation() {
-        // init population and calculate fitness
-        this.pop = new Population();
-        for (int i = 0; i < this.popsize; i++) {
-                double child[] = {
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0
-                };
-                for (int j = 0; j < 10; j++) {
-                        child[j] = rnd_.nextDouble() * 10 - 5;
+
+        public void setEvaluation(ContestEvaluation evaluation) {
+                // Set evaluation problem used in the run
+                evaluation_ = evaluation;
+                // Get evaluation properties
+                Properties props = evaluation.getProperties();
+                // Get evaluation limit
+                evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+                // Property keys depend on specific evaluation
+                // E.g. double param =
+                // Double.parseDouble(props.getProperty("property_name"));
+                boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
+                boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
+                boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+                // Do sth with property values, e.g. specify relevant settings of your
+                // algorithm
+                if (isMultimodal) {
+                        // Do sth
+                } else {
+                        // Do sth else
                 }
-                Individual newIndividual = new Individual(child);
-                newIndividual.setFitness((double) evaluation_.evaluate(child));
-                pop.addIndividual(newIndividual);
         }
-}
-public void run() {
-        // Run your algorithm here
-        int evals = 0;
-        // init population, population size is 10
-        this.popsize = 10;
-        this.InitPopulation();
-        double p_mutation = 0.2, p_crossover = 1; //Probability of mutation and crossover
-        EvoAlgorithm evo = new EvoAlgorithm(pop, p_mutation, p_crossover);
-        while (evals < evaluations_limit_ - this.popsize) {
-                // evo.ShowBestFitness();
-                // Select parents
-                // evo.Select_RW();			      //Roulette Wheel Selection
-                evo.Select_TS(4);             // Tournament Selection(size of the Tournament)
-                // Apply crossover / mutation operators
-                //evo.Crossover_SA(0.5);        // Simple arithmetic crossover
-                evo.Crossover_WA(0.5);      //Whole arithmetic crossover
-                evo.Mutation(0.05); //randomGauss(sigma)
-                // Check fitness of unknown fuction
-                Population offspring = evo.getOffspring();
-                for (int i = 0; i < offspring.getPopulation().size(); i++) {
-                        Individual child = offspring.getPopulation().get(i);
-                        double fitness = (double) this.evaluation_.evaluate(child.getGenes());
-                        offspring.getPopulation().get(i).setFitness(fitness);
-                        evals++;
+
+        public void InitPopulation() {
+                // init population and calculate fitness
+                this.pop = new Population();
+                for (int i = 0; i < this.popsize; i++) {
+                        double child[] = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+                        for (int j = 0; j < 10; j++) {
+                                child[j] = rnd_.nextDouble() * 10 - 5;
+                        }
+                        Individual newIndividual = new Individual(child);
+                        newIndividual.setFitness((double) evaluation_.evaluate(child));
+                        pop.addIndividual(newIndividual);
                 }
-                evo.setOffspring(offspring);
-                // Select survivors
-                evo.Survive();
         }
-}
+
+        public void run() {
+                // Run your algorithm here
+                int evals = 0;
+                // init population, population size is 10
+                this.popsize = 10;
+                this.InitPopulation();
+                int sizeOfT = 4;
+                double p_mutation = 0.2, p_crossover = 1, mixRate = 0.5, msigma = 0.02; // Probability of mutation and
+                                                                                          // crossover
+                EvoAlgorithm evo = new EvoAlgorithm(pop, evaluation_, p_mutation, p_crossover, evaluations_limit_ - this.popsize,
+                                sizeOfT, mixRate, msigma);
+                evo.run();
+        }
 }
